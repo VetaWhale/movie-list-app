fe1647cbb7972fbb0c35eb97e8b06287cd9358a8
Elizaveta Shkimbova <veta-whale@yandex.ru>
2023-06-23 17:43:32 +0300

    saved to LocalStorage

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const formNode = document.querySelector('#form-film');
 const buttonAddFilm = document.querySelector('.button-add');
 const filmsList = document.querySelector('.films-list-wrapper');
 
+let films = [];
+
+// renderToLocalStorage();
+if (localStorage.getItem('films')) {
+    films = JSON.parse(localStorage.getItem('films'));
+    console.log(films)
+}
+
+films.forEach(film => renderFilm(film));
+
 buttonAddFilm.addEventListener('click', addFilm);
 filmsList.addEventListener('click', viewFilm);
 filmsList.addEventListener('click', deleteFilm);
@@ -11,13 +21,17 @@ function addFilm() {
 
     if (!film) return;
 
-    const filmHTML = `<li class="film-item">
-    <input type="checkbox" name="" id="" class="checkbox" data-action="check">
-    <p class="title-film">${film}</p>
-    <button class="close" data-action="delete"><img src="img/close.svg" alt="" /></button>
-    </li>`
+    const newFilm = {
+        id: Date.now(),
+        name: film,
+        check: false,
+    }
 
-    filmsList.insertAdjacentHTML('beforeend', filmHTML);
+    films.push(newFilm);
+
+    renderFilm(newFilm);
+
+    saveToLocalStorage();
 
     formNode.value = "";
 }
@@ -25,13 +39,68 @@ function addFilm() {
 function deleteFilm(event) {
     if (event.target.dataset.action === 'delete') {
         const parentNode = event.target.closest('.film-item');
+
+        const id = Number(parentNode.id);
+        
+        const index = films.findIndex(function (film) {
+            if (film.id === id) {
+                return true;
+            }
+        })
+
+        films.splice(index, 1);
+
         parentNode.remove();
+
+        saveToLocalStorage();
     }
 }
 
 function viewFilm(event) {
     if (event.target.dataset.action === 'check') {
         const parentNode = event.target.closest('.film-item');
+        const parentCheckNode = event.target.closest('.checkbox');
+
+        const id = Number(parentNode.id);
+        
+        const filmDone = films.find(function (film) {
+            if (film.id === id) {
+                return true;
+            }
+        })
+        
+
+        filmDone.check = !filmDone.check;
+
         parentNode.classList.toggle('active-film-item');
+        parentCheckNode.classList.toggle('active-checkbox');
+
+        saveToLocalStorage();
     }
+    
 }
+
+function saveToLocalStorage() {
+    localStorage.setItem('films', JSON.stringify(films))
+}
+
+function renderFilm(film) {
+    const classFilm = film.check ? "film-item active-film-item" : "film-item";
+    const classCheckboxFilm = film.check ? "checkbox active-checkbox" : "checkbox";
+
+    const filmHTML = `<li class="${classFilm}" id="${film.id}">
+    <input type="checkbox" name="" id="" class="${classCheckboxFilm}" data-action="check">
+    <p class="title-film">${film.name}</p>
+    <button class="close" data-action="delete"><img src="img/close.svg" alt="" /></button>
+    </li>`
+
+    filmsList.insertAdjacentHTML('beforeend', filmHTML);
+}
+
+// function renderToLocalStorage() {
+//    if (localStorage.getItem('films')) {
+//         films = JSON.parse(localStorage.getItem('films'));
+//     }
+    
+//     films.forEach(film => renderFilm(film)); 
+// }
